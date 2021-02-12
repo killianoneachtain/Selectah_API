2698910373fcc8900acf4fadae4d55fd6448ade5
Killian O'Neachtain <killianoneachtain@hotmail.com>
2021-02-12 21:14:51 +0000

    CORS / NO-CORS implementation

--- routes/identity.js
+++ routes/identity.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var Discogs = require('disconnect').Client;
+const cors = require('cors');
+
+
+app.get('/with-cors', cors(), (req, res, next) => {
+  res.json({ msg: 'WHOAH with CORS it works! 🔝 🎉' })
+})
 
 app.get('/identity', function(req, res){
 	var dis = new Discogs(accessData);
