870a4ac3868d04a72210ba166f901184f9312ce6
Killian O'Neachtain <killianoneachtain@hotmail.com>
2021-02-18 21:04:15 +0000

    New routes added to allow for releases to be returned

---
+++ node_modules/ends-with-string/README.md
@@ -0,0 +1,58 @@
+<div align="center">
+  <h1>ends-with-string</h1>
+  <a href="https://npmjs.com/package/ends-with-string">
+    <img alt="NPM" src="https://img.shields.io/npm/v/ends-with-string.svg">
+  </a>
+  <a href="https://github.com/bconnorwhite/ends-with-string">
+    <img alt="TypeScript" src="https://img.shields.io/github/languages/top/bconnorwhite/ends-with-string.svg">
+  </a>
+  <a href='https://coveralls.io/github/bconnorwhite/ends-with-string?branch=master'>
+    <img alt="Coverage Status" src="https://img.shields.io/coveralls/github/bconnorwhite/ends-with-string.svg?branch=master">
+  </a>
+  <a href="https://github.com/bconnorwhite/ends-with-string">
+    <img alt="GitHub Stars" src="https://img.shields.io/github/stars/bconnorwhite/ends-with-string?label=Stars%20Appreciated%21&style=social">
+  </a>
+  <a href="https://twitter.com/bconnorwhite">
+    <img alt="Twitter Follow" src="https://img.shields.io/twitter/follow/bconnorwhite.svg?label=%40bconnorwhite&style=social">
+  </a>
+</div>
+
+<br />
+
+> Check if a string or buffer ends with a given string.
+
+## Installation
+
+```sh
+yarn add ends-with-string
+```
+
+```sh
+npm install ends-with-string
+```
+
+## API
+
+```ts
+import endsWith from "ends-with-string";
+
+function endsWith(input: string | Buffer, string: string): boolean;
+```
+
+<br />
+
+<h2>Dependencies<img align="right" alt="dependencies" src="https://img.shields.io/david/bconnorwhite/ends-with-string.svg"></h2>
+
+- [char-to-string](https://www.npmjs.com/package/char-to-string): Convert a character or character code to string
+
+<br />
+
+<h2>Dev Dependencies<img align="right" alt="David" src="https://img.shields.io/david/dev/bconnorwhite/ends-with-string.svg"></h2>
+
+- [@bconnorwhite/bob](https://www.npmjs.com/package/@bconnorwhite/bob): Bob is a tookit for TypeScript projects
+
+<br />
+
+<h2>License <img align="right" alt="license" src="https://img.shields.io/npm/l/ends-with-string.svg"></h2>
+
+[MIT](https://opensource.org/licenses/MIT)
--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,17 @@ var express = require('express');
 var router = express.Router();
 var Discogs = require('disconnect').Client;
 const cors = require('cors');
+const fs = require('fs');
+var db = new Discogs().database();
 
-var collectorsItems;
-var noOfPages;
-var pagination;
+var dis = new Discogs({
+	consumerKey: 'rSlgSvPFNYXtkYClvjLs', 
+	consumerSecret: 'QRabRHFedozJinKOvopMUCKeCepaCJLn'
+});
+
+var collectorsItems= [];
+var noOfPages=0;
+var completeCollection=[];
 
 /* GET users listing. */
 router.get('/', cors(), function(req, res, next) {
@@ -15,18 +22,38 @@ router.get('/', cors(), function(req, res, next) {
 router.get('/collection', cors(), function(req, res, next) {  
   var col = new Discogs({userToken: 'lYVtKyeISQGrTaFWvhONqkFfvbexIAIsrNiJhvAf'}).user().collection();
   
-  col.getReleases('konsouloz', 0, {per_page:1000},
+  col.getReleases('konsouloz', 0, {per_page:50},
    function(err, data){
-      //console.log(data);
-      //pagination =res.json(data.pagination);
-      //console.log("Number of Pages: "+pagination.pages);
-      res.json(data.releases);//
-    });
-  
+        /*console.log(data);        
+        noOfPages =data.pagination.pages;        
+        console.log("Number of Pages: "+noOfPages);
+        if(parseInt(noOfPages) > 1)
+        {
+          for(i = 2;i<=noOfPages;i++)
+          {
+            col.getReleases('konsouloz', 0, {page: i,per_page:500},
+            function(err, data)
+            {
+              collectorsItems.push(data.releases);
+            });
+          }
+        } else{ collectorsItems.push(data.releases)}
+    });   
+  res.json(collectorsItems);*/
+ 
+     console.log(data);
+     res.json(data.releases);
+   });
 });
 
+/* GET releases track listing. */
+router.get('/release/:releaseId', cors(), function(req, res, next) {
+  var dis = new Discogs('MyUserAgent/1.0', {userToken: 'lYVtKyeISQGrTaFWvhONqkFfvbexIAIsrNiJhvAf'});
+   db.getRelease(req.params.releaseId, function(err, data){
+    //console.log(data);
+    res.json(data);
+  });  
+});
+
+
 module.exports = router;
-
-function getEntireCollection(string ,res){
-
-}
