ad0c39f6c7496530227c12b1bf2e37a02ea0bd84
Killian O'Neachtain <killianoneachtain@hotmail.com>
2021-02-20 21:12:12 +0000

    Return only track-list array from releases info

--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,7 @@ var dis = new Discogs({
 
 var collectorsItems= [];
 var noOfPages=0;
-var completeCollection=[];
+var completeCollection="";
 
 /* GET users listing. */
 router.get('/', cors(), function(req, res, next) {
@@ -20,40 +20,65 @@ router.get('/', cors(), function(req, res, next) {
 });
 
 router.get('/collection', cors(), function(req, res, next) {  
-  var col = new Discogs({userToken: 'lYVtKyeISQGrTaFWvhONqkFfvbexIAIsrNiJhvAf'}).user().collection();
+    var col = new Discogs({userToken: 'lYVtKyeISQGrTaFWvhONqkFfvbexIAIsrNiJhvAf'}).user().collection();
   
-  col.getReleases('konsouloz', 0, {per_page:50},
-   function(err, data){
-        /*console.log(data);        
-        noOfPages =data.pagination.pages;        
-        console.log("Number of Pages: "+noOfPages);
-        if(parseInt(noOfPages) > 1)
-        {
-          for(i = 2;i<=noOfPages;i++)
-          {
-            col.getReleases('konsouloz', 0, {page: i,per_page:500},
-            function(err, data)
-            {
-              collectorsItems.push(data.releases);
-            });
-          }
-        } else{ collectorsItems.push(data.releases)}
-    });   
-  res.json(collectorsItems);*/
- 
-     console.log(data);
-     res.json(data.releases);
-   });
+    col.getReleases('konsouloz', 0, {per_page:50},
+    function(err, data){
+        noOfPages =data.pagination.pages;   
+        //console.log(noOfPages);   
+        //completeCollection = getAllReleases(parseInt(noOfPages))
+        res.json(data.releases); 
+        //res.json(data.releases);
+    });  
 });
 
-/* GET releases track listing. */
+function getAllReleases(pages)
+{
+     //noOfPages =data.pagination.pages;   
+        //console.log(noOfPages);
+  var col = new Discogs({userToken: 'lYVtKyeISQGrTaFWvhONqkFfvbexIAIsrNiJhvAf'}).user().collection();
+
+  var strings="";
+  var news="";
+  var together = "";
+  console.log(" I is : ", pages);
+
+    for(i = 1;i<=pages;i++)
+    {
+    //console.log("length of strings",strings.length);
+     col.getReleases('konsouloz', 0, {page: `${i}`, per_page:50},
+    function(err, data){
+        news = JSON.stringify(data.releases); 
+        news = news.substring(1);
+        var n = news.lastIndexOf("]");
+        news = news.substring(0,n) + ',';
+        console.log("length of news now is : ",news.length);     
+        strings += news;   
+        console.log("length of strings",strings.length); 
+        
+        console.log("i : ", i);
+        console.log("pages", pages);
+        
+        if(i>pages)
+        {
+          var l = strings.length;
+          var set = `[${strings}]`;
+          strings = JSON.parse(set);
+          console.log("SET : ",strings);
+        } 
+        });  
+    } 
+    return strings;  
+   };
+
+   /* GET releases track listing. */
 router.get('/release/:releaseId', cors(), function(req, res, next) {
   var dis = new Discogs('MyUserAgent/1.0', {userToken: 'lYVtKyeISQGrTaFWvhONqkFfvbexIAIsrNiJhvAf'});
    db.getRelease(req.params.releaseId, function(err, data){
     //console.log(data);
-    res.json(data);
+    // cache and timestamp
+    res.json(data.tracklist);
   });  
 });
-
 
 module.exports = router;
